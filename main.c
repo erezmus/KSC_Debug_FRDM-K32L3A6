31e1077804975562acd190c5f74fe664691752a2
GuentherMartin <martin.guenther@arm.com>
2021-05-05 16:34:30 +0200

    Added pointer variable.

--- main.c
+++ main.c
@@ -35,28 +35,30 @@ static void delay (int32_t dlyTicks)
 }
 #endif
 
-static float    gVarF32_s = 1.32f;
-       float    gVarF32   = 0.32f;
-       double   gVarD64   = 0.64;
-       int64_t  gVarS64   = 64;
-       int32_t  gVarS32   = 32;
-       uint32_t gVarU32   = 32U;
-       uint16_t gVarU16   = 16U;
-       uint8_t  gVarU8    = 8U;
-       char     gText[]   = "Hello World.";
+static float     gVarF32_s = 1.32f;
+       float     gVarF32   = 0.32f;
+       double    gVarD64   = 0.64;
+       int64_t   gVarS64   = 64;
+       int32_t   gVarS32   = 32;
+       uint32_t  gVarU32   = 32U;
+       uint32_t *gVarU32_p = &gVarU32;
+       uint16_t  gVarU16   = 16U;
+       uint8_t   gVarU8    = 8U;
+       char      gText[]   = "Hello World.";
 
 
 int main (void)
 {
-  volatile float    lVarF32 = gVarF32;
-  volatile double   lVarD64 = gVarD64;
-  volatile int64_t  lVarS64 = gVarS64;
-  volatile int32_t  lVarS32 = gVarS32;
-  volatile uint32_t lVarU32 = gVarU32;
-  volatile uint16_t lVarU16 = gVarU16;
-  volatile uint8_t  lVarU8  = gVarU8;
-  volatile size_t   lVarST  = 0;
-  volatile char     lText[20];
+  volatile float     lVarF32   = gVarF32;
+  volatile double    lVarD64   = gVarD64;
+  volatile int64_t   lVarS64   = gVarS64;
+  volatile int32_t   lVarS32   = gVarS32;
+  volatile uint32_t  lVarU32   = gVarU32;
+  volatile uint32_t *lVarU32_p = gVarU32_p;
+  volatile uint16_t  lVarU16   = gVarU16;
+  volatile uint8_t   lVarU8    = gVarU8;
+  volatile size_t    lVarST    = 0;
+  volatile char      lText[20];
 
  __DSB();
 
@@ -68,6 +70,9 @@ int main (void)
 
   gVarS32 = INT32_MAX;
   lVarU32 = sfiUSAT(gVarS32, 8U);
+
+  lVarU32_p = &lVarU32;
+  *gVarU32_p = *lVarU32_p;
 
   lVarS64 = lVarS32 = gVarS32;
   lVarS64 = lVarS64 * lVarS32;
